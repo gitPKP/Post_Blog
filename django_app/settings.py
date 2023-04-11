736b5417efe94f4d935bf3fc841725f36819f55d
gitPKP <130458060+gitPKP@users.noreply.github.com>
2023-04-11 17:36:48 +0200

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -36,7 +36,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.post-blog-tomu.onrender.com']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 LOGGING = {
    'version': 1,
